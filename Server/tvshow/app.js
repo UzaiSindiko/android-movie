a54dc712328d7caafed50eef3e82f2d51c806a26
uzaisindiko <uzai.sindiko@gmail.com>
2019-12-11 10:02:53 +0700

    change port

--- Server/tvshow/app.js
+++ Server/tvshow/app.js
@@ -11,7 +11,7 @@ const errorHandler = require('./middleware/errorHandler')
 
 const app = express()
 
-const PORT = process.env.PORT || 3002
+const PORT = 3002
 const URI = process.env.URI || 'mongodb://localhost:27017/Entertainme==>'
 
 app.use(express.urlencoded({extended: false}))
