d5e4789aaa69a6b9ad0a787d51671de7ce8fd056
uzaisindiko <uzai.sindiko@gmail.com>
2019-12-11 09:00:53 +0700

    change port and database Ready Deploy

--- Server/orchestratorGraphQL/index.js
+++ Server/orchestratorGraphQL/index.js
@@ -1,6 +1,12 @@
+if(process.env.NODE_ENV === 'development'){
+  require('dotenv').config()
+}
+
 const { ApolloServer, gql, makeExecutableSchema } = require('apollo-server');
 const { typeDefsMovie, resolversMovie } = require('./Schema/movieSchema')
 const {typeDefsTVShow, resolversTVShow} = require('./Schema/tvshowSchema')
+
+const PORT = process.env.PORT
 
 const typeDefs = gql`
   type Query,
@@ -15,6 +21,6 @@ const schema = makeExecutableSchema({
 const server = new ApolloServer({ schema });
 
 // The `listen` method launches a web server.
-server.listen().then(({ url }) => {
+server.listen(PORT).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
